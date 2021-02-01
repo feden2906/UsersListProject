9bdfa61b2add53f37abc5247ecb17a85c0296957
Fedenko Maksym <feden2906@gmail.com>
2021-02-01 18:25:52 +0200

    fix update users list

--- src/Redux/reducers/user-reducer.js
+++ src/Redux/reducers/user-reducer.js
@@ -1,34 +1,30 @@
 import {ADD_CHOSEN_USER, ADD_USERS_FROM_API, CREATE_USER, DELETE_CHOSEN_USER, SAVE_USERS_LIST} from "../action-types";
 
 
-const initialState = {
-    users: [],
-    chosenUser: []
-}
+const initialState = {users: [], chosenUser: []}
+
 
 export const reducer = (state = initialState, action) => {
-    switch (action.type) {
-        case ADD_USERS_FROM_API: {
-            return {...state, users: action.payload}
-        }
-        case ADD_CHOSEN_USER: {
-            return {...state, chosenUser: action.payload}
-        }
-        case DELETE_CHOSEN_USER: {
-            const withDeleted = state.users.filter(user => user.id !== action.payload);
-            return {...state, users: withDeleted}
-        }
-        case SAVE_USERS_LIST: {
-            return {...state, users: action.payload}
-        }
-        case CREATE_USER: {
-            console.log(action.payload);
-            state.users.push(action.payload)
-            console.log(state)
-            return state;
-        }
-        default: {
-            return state;
-        }
+  const {type, payload} = action
+  switch (type) {
+    case ADD_USERS_FROM_API: {
+      return {...state, users: payload}
     }
+    case ADD_CHOSEN_USER: {
+      return {...state, chosenUser: payload}
+    }
+    case DELETE_CHOSEN_USER: {
+      const withDeleted = state.users.filter(user => user.id !== payload);
+      return {...state, users: withDeleted}
+    }
+    case SAVE_USERS_LIST: {
+      return {...state, users: payload}
+    }
+    case CREATE_USER: {
+      return {...state, users: [...state.users, payload]};
+    }
+    default: {
+      return state;
+    }
+  }
 }
